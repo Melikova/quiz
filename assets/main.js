9d7bd4527d20d7899de15256e28e7a2e956bc30c
Mehin Melikova <mhmelikova@gmail.com>
2023-06-12 17:52:16 +0400

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,80 @@
-// $.getJSON("./data/js_questions_1.json", (data)=>{
-//     console.log(data);
-// }).fail(function(err){
-//     console.log(err);
-// });
+$('#main').html(`<div id="question">${data.quiz[0].question}</div><div id="answers">${generate_answers(data.quiz[0].answers)}</div>`);
+$('button').attr("disabled", true);
+let wrong_answer=[];
+let wrong_answers=[];
+let answer_id;
+choose_answer();
 
-async function logJSONData() {
-    const response = await fetch("../data/js_questions_1.json");
-    const jsonData = await response.json();
-    console.log(jsonData);
+
+const gen = content_maker();
+
+$('.next').on('click', function(){
+  $('button').attr("disabled", true);
+  $('#main').html(gen.next().value);
+  $(this).removeClass('selected');
+  choose_answer();
+  $('.restart').on('click', function(){
+    location.reload(true);
+  })
+  
+})
+
+function choose_answer(){
+  $('#answers div').each(function(){
+    $(this).on('click', function(){
+      $('button').attr("disabled", false);
+      $('#answers div').each(function(){
+        $(this).removeClass('selected')
+      });
+      $(this).addClass('selected');
+      $('.next').addClass('selected');
+      answer_id = $(this).data('id');
+      if(answer_id !=='a'){
+        wrong_answer=[$(this).parent().prev().text(), $(this).children()[1].outerText];
+      }else{
+        wrong_answer=[];
+      }
+    })
+  })
+  wrong_answers.push(wrong_answer);
+}
+
+function* content_maker() {
+  let main_content;
+  for(let i=1; i<data.quiz.length; i++){
+    main_content=`<div id="question">${data.quiz[i].question}</div><div id="answers">${generate_answers(data.quiz[i].answers)}</div>`;
+    yield main_content;
   }
+  const filteredArray = wrong_answers.filter( item => item.length !== 0);
+  let result='';
+  filteredArray.forEach(val=>{
+    result+= `<li class="list-group-item d-flex justify-content-between align-items-start mt-3">
+      <div class="ms-2 me-auto">
+        <div class="fw-bold">${val[0]}</div>
+        Your wrong answer: ${val[1]}
+      </div>
+    </li>`;
+  })
+  main_content=`<div> 
+    Total: 10 questions; 
+    Right answers: ${wrong_answers.length - filteredArray.length}; 
+    Wrong answers: ${filteredArray.length};
+    <ol class="list-group list-group-numbered">${result}</ol>
+  </div>`;
+  $('.next').text('RESTART');
+  $('.next').addClass('restart');
+  $('button').attr("disabled", false);
+  yield main_content;
+}
 
-  logJSONData();
+function generate_answers(object_data){
+  const index_array=['a', 'b', 'c', 'd'];
+  const array= Object.entries(object_data);
+  const shuffledArray = array.sort((a, b) => 0.5 - Math.random());
+  let answers='';
+  shuffledArray.forEach((val, key)=>{
+    answers+=`<div data-id="${val[0]}"><div class="index">${index_array[key].toUpperCase()}</div><span class="answer">${val[1]}</span></div>`;
+  });
+  return answers;
+}
+
---
+++ data/data.js
@@ -0,0 +1,95 @@
+let data = {
+    "quiz": [
+      {
+        "question": "What are the primitive data types in JavaScript?",
+        "answers": {
+          "a": "Strings, numbers, booleans, null, undefined",
+          "b": "Arrays, objects, functions, symbols",
+          "c": "Dates, regular expressions, maps, sets",
+          "d": "Promises, iterators, generators, buffers"
+        }
+      },
+      {
+        "question": "Explain the difference between null and undefined.",
+        "answers": {
+          "a": "null represents an intentional absence of any object value, while undefined represents an uninitialized or missing value.",
+          "b": "null is used for numeric calculations, while undefined is used for string manipulations.",
+          "c": "null is a truthy value, while undefined is a falsy value.",
+          "d": "null can be converted to a string, while undefined cannot be converted."
+        }
+      },
+      {
+        "question": "What is variable hoisting in JavaScript?",
+        "answers": {
+          "a": "The process of moving variables to the top of their scope before code execution.",
+          "b": "The automatic conversion of variables into their primitive data types.",
+          "c": "The practice of initializing variables with the keyword 'hoist'.",
+          "d": "The ability to reassign variables multiple times within the same block."
+        }
+      },
+      {
+        "question": "Describe the differences between var, let, and const keywords in JavaScript.",
+        "answers": {
+          "a": "var is function-scoped, let is block-scoped, and const is block-scoped and requires initialization with a value.",
+          "b": "var is block-scoped, let is function-scoped, and const is function-scoped and requires initialization with a value.",
+          "c": "var is block-scoped, let is block-scoped, and const is function-scoped.",
+          "d": "var is function-scoped, let is function-scoped, and const is block-scoped and requires initialization with a value."
+        }
+      },
+      {
+        "question": "What are the differences between pass-by-value and pass-by-reference in JavaScript?",
+        "answers": {
+          "a": "In pass-by-value, a copy of the value is passed, while in pass-by-reference, the reference to the value is passed.",
+          "b": "In pass-by-value, the reference to the value is passed, while in pass-by-reference, a copy of the value is passed.",
+          "c": "Pass-by-value and pass-by-reference are two terms for the same concept in JavaScript.",
+          "d": "Pass-by-value and pass-by-reference are not applicable concepts in JavaScript."
+        }
+      },
+      {
+        "question": "How do you check the type of a variable in JavaScript?",
+        "answers": {
+          "a": "By using the typeof operator.",
+          "b": "By using the type() function.",
+          "c": "By checking the constructor property.",
+          "d": "By using the isType() method."
+        }
+      },
+      {
+        "question": "Explain the concept of type coercion in JavaScript.",
+        "answers": {
+            "a": "Type coercion is the automatic conversion of values from one data type to another.",
+            "b": "Type coercion is the process of explicitly specifying the data type of a variable.",
+            "c": "Type coercion is the practice of assigning multiple data types to a single variable.",
+            "d": "Type coercion is the process of converting a string into a number in JavaScript."
+        }
+      },
+      {
+        "question": "What is event delegation in JavaScript?",
+        "answers": {
+          "a": "Event delegation is the process of associating an event with multiple elements simultaneously.",
+          "b": "Event delegation is the practice of capturing events before they propagate to the target element.",
+          "c": "Event delegation is the process of handling events on parent elements instead of individual child elements.",
+          "d": "Event delegation is the practice of delaying event handling until all other tasks are complete."
+        }
+      },
+      {
+        "question": "What is the purpose of the 'use strict' directive in JavaScript?",
+        "answers": {
+          "a": "The 'use strict' directive enforces stricter parsing and error handling in JavaScript code.",
+          "b": "The 'use strict' directive enables the use of modern JavaScript features in older browsers.",
+          "c": "The 'use strict' directive restricts the use of certain keywords and functionalities in JavaScript code.",
+          "d": "The 'use strict' directive is a comment that has no effect on JavaScript code execution."
+        }
+      },
+      {
+        "question": "What is the difference between synchronous and asynchronous JavaScript?",
+        "answers": {
+          "a": "Synchronous JavaScript executes tasks one after the other, while asynchronous JavaScript allows tasks to run independently of each other.",
+          "b": "Synchronous JavaScript is used for server-side programming, while asynchronous JavaScript is used for client-side programming.",
+          "c": "Synchronous JavaScript requires the use of callbacks, while asynchronous JavaScript uses promises or async/await.",
+          "d": "Synchronous JavaScript is more efficient than asynchronous JavaScript in terms of performance."
+        }
+      }
+    ]
+}
+            
